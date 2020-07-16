6f69fc211302dbaa207012e8f76ad5a46ab4960b
igacourse <66384396+igacourse@users.noreply.github.com>
2020-07-16 12:11:40 +0200

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,10 @@
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const containers = document.getElementById('containers');
+signUpButton.addEventListener('click', () => {
+	containers.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	containers.classList.remove("right-panel-active");
+});
